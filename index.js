5381e00a63ee531d8802351e467d483db71dd043
Kazuki Noto <sales-demo@vinx.co.jp>
2019-02-23 18:05:05 +0900

    json type変更

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var ItemJson = { "item": "no item" }
 
 var server = http.createServer(function (request, response) {
 
-    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.writeHead(200, { "Content-Type": "application/json" });
     response.end(ItemJson);
 
 });
